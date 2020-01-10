240129e8a23057204b8b593dbb525d89f182e4a0
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-10 14:25:46 -0600

    bigUpdate-HTML-CSS-gitIgnore-javascript

---
+++ src/main.js
@@ -0,0 +1,15 @@
+var krafftWord = 'KurtRules1'
+
+
+var checkPassword = () => {
+    var pWord = document.getElementById("pWord").value
+    // console.log(pWord)
+    if (pWord == krafftWord){
+        alert ("hey you got it buddy!")
+    } else {
+        alert("Sorry, try again mane")
+    }
+}
+var joiningKrew = () => {
+    alert ("Thanks for trying, unfortunately, this function isn't working yet :(")
+}

d88463faeb37d1048bc8af51cc1a76b54fee0adc
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-20 14:37:26 -0600

    added basic additions on seperate JS sheet

--- scripts.js
+++ scripts.js
@@ -24,3 +24,21 @@ const journalCollection = [];
 journalCollection.push(newEntry);
 journalCollection.push(newEntryTwo);
 journalCollection.push(newEntryThree);
+
+const generateHTML = (journal) =>{
+    let html = `<div class="newEntries"><div class='newEntry'>
+    <h3>${journal.topic}</h3>
+    <h5 class="entryItem">Date: ${journal.date}</h5>
+    <h5 class="entryItem">Mood: ${journal.mood}</h5>
+    <h5 class="entryItem">Entry: ${journal.textarea}</h5></div></div>`
+    return html;
+}
+
+const logEntry = () =>{
+    for (let i=0; i<journalCollection.length;i++){
+        const entrySection = document.querySelector('.entrySection');
+        entrySection.innerHTML+= generateHTML(journalCollection[i]);
+    }
+}
+
+logEntry();
